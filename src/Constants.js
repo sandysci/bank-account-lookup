aba48cb82aca27f916ae89212f60f34bb39c2725
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2020-01-01 18:19:01 +0100

    Added Constants

---
+++ src/Constants.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+module.exports = {
+    ROLE: {
+        ADMIN: "admin",
+        USER: "user",
+    },
+    SECURITY: {
+        INTERNAL_SECURITY_TOKEN: "INTERNAL_SECURITY_TOKEN",
+    },
+    CLIENT_DEFAULT: "default",
+    SECRET_DEFAULT: "default",
+    EXCHANGES: {},
+    QUEUES: {}
+};
