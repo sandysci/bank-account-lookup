baa0664810b196fdd0a6ec816aaba24c8481baee
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2020-01-03 09:59:23 +0100

    Added Events to constants

--- src/Constants.js
+++ src/Constants.js
@@ -11,6 +11,17 @@ module.exports = {
     },
     CLIENT_DEFAULT: "default",
     SECRET_DEFAULT: "default",
-    EXCHANGES: {},
+    EVENTS: {
+        USER:{
+            CREATED: "user_created",
+            UPDATED: "user_updated",
+            DELETED: "user_deleted"
+        },
+        PRODUCT: {
+            CREATED: "product_created",
+            UPDATED: "product_updated",
+            DELETED: "product_deleted"
+        }
+    },
     QUEUES: {}
 };
